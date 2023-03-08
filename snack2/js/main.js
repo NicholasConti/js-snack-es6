998a288a6b92c46a5a265c1387aeb03e6b14349e
NicholasConti <conti94@live.it>
2023-03-08 16:31:38 +0100

    array snack2

---
+++ snack2/js/main.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const studenti = [
+    {
+        id: 213,
+        nome: 'Marco della Rovere',
+        grade: 78,
+    },
+    {
+        id: 110,
+        nome: 'Paola Cortellessa',
+        grade: 96,
+    },
+    {
+        id: 250,
+        nome: 'Andrea Mantegna',
+        grade: 48,
+    },
+    {
+        id: 145,
+        nome: 'Gaia Borromini',
+        grade: 74,
+    },
+    {
+        id: 196,
+        nome: 'Luigi Grimaldello',
+        grade: 68,
+    },
+    {
+        id: 102,
+        nome: 'Piero della Francesca',
+        grade: 50,
+    },
+    {
+        id: 120,
+        nome:'Francesca da Polenta',
+        grade: 84,
+    }
+];
