7f7878681f70088dea5f24c252840ba0d459ee05
NicholasConti <conti94@live.it>
2023-03-08 16:08:59 +0100

    array di oggetti

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const invitati = [
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Brad Pitt',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Johnny Depp',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Lady Gaga',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Cristiano Ronaldo',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Georgina Rodriguez',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Chiara Ferragni',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Fedez',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'George Clooney',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Amal Clooney',
+    },
+    {
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: 'Maneskin',
+    }
+];
+
