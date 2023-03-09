ed2f0ae4e137d627b4d9e37294873a769f75a8f0
NicholasConti <conti94@live.it>
2023-03-09 16:09:14 +0100

    array snack3

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const bikes = [
+    {
+        brand: 'Giant',
+        kg: 11
+    },
+    {
+        brand: 'Trek',
+        kg: 9
+    },
+    {
+        brand: 'Cube',
+        kg: 8
+    },
+    {
+        brand: 'Focus',
+        kg: 7
+    },
+    {
+        brand: 'Canondale',
+        kg: 6
+    },
+    {
+        brand: 'Cervelo',
+        kg: 10
+    },
+    {
+        brand: 'Specialized',
+        kg: 13
+    }
+];
+
