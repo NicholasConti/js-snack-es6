d6bfdea9c88dfc93708dda84589c364a75c21525
NicholasConti <conti94@live.it>
2023-03-09 10:18:49 +0100

    fix

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,48 +1,23 @@
 "use strict";
-
 const invitati = [
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Brad Pitt',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Johnny Depp',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Lady Gaga',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Cristiano Ronaldo',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Georgina Rodriguez',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Chiara Ferragni',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Fedez',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'George Clooney',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Amal Clooney',
-    },
-    {
-        nomeTavolo: 'Tavolo VIP',
-        nomeInvitato: 'Maneskin',
-    }
+    'Brad Pitt',
+    'Johnny Depp',
+    'Lady Gaga',
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin'
 ];
 
-invitati.forEach((element, index) => {
-    console.log(element.nomeTavolo, element.nomeInvitato, index + 1);
-})
+
+const listaInvitati = invitati.map((element, index) => {
+    return{
+        nomeTavolo: 'Tavolo VIP',
+        nomeInvitato: element,
+        segnaposto: index + 1,
+    }
+});
+console.log(listaInvitati);
