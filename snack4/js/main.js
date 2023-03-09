185c3a0ca2db25bac4e804d25d4a13aca94f4762
NicholasConti <conti94@live.it>
2023-03-09 16:51:01 +0100

    array-teams Snack4

---
+++ snack4/js/main.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const serieA = [
+    {
+        nome: 'Atalanta',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Bologna',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Cremonese',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Empoli',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Fiorentina',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Hellas Verona',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Juventus',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Lazio',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Lecce',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Monza',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Napoli',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Roma',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Salernitana',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Sampdoria',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Sassuolo',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Spezia',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Torino',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'Udinese',
+        punti: 0,
+        falli: 0
+    }
+];
